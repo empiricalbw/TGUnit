691f15e74fe41608317cf976d52f4d14df7d70eb
Empirical <empirical.bw@gmail.com>
2020-10-01 17:55:23 -0600

    TGTest.lua: Display health changes.

--- TGTest.lua
+++ TGTest.lua
@@ -18,6 +18,11 @@ function TGTestObject:UPDATE_NAME(unit)
     TGDbg(unit.id..":name is now "..tostring(unit.name))
 end
 
+function TGTestObject:UPDATE_HEALTH(unit)
+    TGDbg(unit.id..":health is now ["..tostring(unit.health.current).."/"..
+          tostring(unit.health.max).."]")
+end
+
 function TGTestObject:UPDATE_POWER(unit)
     TGDbg(unit.id..":power is now "..tostring(unit.power.type).."["..
           tostring(unit.power.current).."/"..tostring(unit.power.max).."]")
