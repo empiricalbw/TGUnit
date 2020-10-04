a5d3073dbc914a28eb9926c9f39b3041a31bcda8
Empirical <empirical.bw@gmail.com>
2020-10-03 20:26:51 -0600

    TGDebugging.lua: Add TGMsg function that is independent of TGDbg.

--- TGDebugging.lua
+++ TGDebugging.lua
@@ -3,6 +3,11 @@ function TGDbg(str)
     DEFAULT_CHAT_FRAME:AddMessage(s)
 end
 
+function TGMsg(str)
+    local s = LIGHTYELLOW_FONT_COLOR_CODE.."TGMsg: "..FONT_COLOR_CODE_CLOSE..str
+    DEFAULT_CHAT_FRAME:AddMessage(s)
+end
+
 TGDbg("Hello, world!")
 
 local function TGDbg_Slash(str)
