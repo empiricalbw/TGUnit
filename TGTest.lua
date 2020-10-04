6c1c9a5970341bc4c9c733bab4baec453ac65704
Empirical <empirical.bw@gmail.com>
2020-10-04 15:17:14 -0600

    TGTest.lua: Disable health and power updates (too much spam).

--- TGTest.lua
+++ TGTest.lua
@@ -19,14 +19,16 @@ function TGTestObject:UPDATE_NAME(unit)
     TGDbg(unit.id..":name is now "..tostring(unit.name))
 end
 
-function TGTestObject:UPDATE_HEALTH(unit)
-    TGDbg(unit.id..":health is now ["..tostring(unit.health.current).."/"..
-          tostring(unit.health.max).."]")
-end
+if false then
+    function TGTestObject:UPDATE_HEALTH(unit)
+        TGDbg(unit.id..":health is now ["..tostring(unit.health.current).."/"..
+              tostring(unit.health.max).."]")
+    end
 
-function TGTestObject:UPDATE_POWER(unit)
-    TGDbg(unit.id..":power is now "..tostring(unit.power.type).."["..
-          tostring(unit.power.current).."/"..tostring(unit.power.max).."]")
+    function TGTestObject:UPDATE_POWER(unit)
+        TGDbg(unit.id..":power is now "..tostring(unit.power.type).."["..
+              tostring(unit.power.current).."/"..tostring(unit.power.max).."]")
+    end
 end
 
 function TGTestObject:UPDATE_LEVEL(unit)
